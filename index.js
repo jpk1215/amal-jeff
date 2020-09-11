abb6d55fcc287d058b9874059325f17cafcc9be5
Jeffrey Klaus <jpk1215@gmail.com>
2020-09-11 15:18:22 -0400

    added morgan

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
 const port = process.env.PORT || 3000
+
+app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
     res.sendFile(`${__dirname}/index.html`);
